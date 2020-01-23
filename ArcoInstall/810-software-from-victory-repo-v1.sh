85064e7aedde4b2eb24c2d22037d0dfbfc8ff8e4
Vicck-tech <vicck.tech@gmail.com>
2020-01-23 09:17:53 -0600

    update

--- ArcoInstall/810-software-from-victory-repo-v1.sh
+++ ArcoInstall/810-software-from-victory-repo-v1.sh
@@ -18,7 +18,7 @@ sudo pacman -S victory-update-git --noconfirm --needed
 sudo pacman -S lightdm-webkit2-greeter --noconfirm --needed
 sudo pacman -S victory-flat-remix-themes-git --noconfirm --needed
 sudo pacman -S victory-lightdm-litarvan-git --noconfirm --needed
+sudo pacman -S victory-plank-git --noconfirm --needed
 sudo pacman -S hefftor-gimp-git --noconfirm --needed
 sudo pacman -S hefftor-variety-git --noconfirm --needed
-
 
